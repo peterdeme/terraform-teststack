2497c4917f7aecb020abc457634926576f6f0089
Peter Deme <snypox@gmail.com>
2022-06-28 16:59:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_ssm_parameter" "testparam" {
     name = "peterdeme-test-parameter"
     type = "String"
-    value = "foobar15"
+    value = "foobar145"
 
     tags = {
         developer = "peterdeme"
