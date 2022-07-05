0f1b654802a66728c7cf0d46274562c1ff4c53b3
Peter Deme <snypox@gmail.com>
2022-07-05 12:53:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,29 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 3.0"
+#   }
+#  }
+#}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
 }
 
-provider "aws" {
-  region = "eu-central-1"
-}
+#provider "aws" {
+#  region = "eu-central-1"
+#}
 
-resource "aws_ssm_parameter" "testparam" {
-    name = "peterdeme-test-parameter"
-    type = "String"
-    value = "foobar"
+#resource "aws_ssm_parameter" "testparam" {
+#    name = "peterdeme-test-parameter"
+#    type = "String"
+#    value = "foobar"
 
-    tags = {
-        developer = "peterdeme"
-        env_name = "development"
-    }
-}
+#    tags = {
+#        developer = "peterdeme"
+#        env_name = "development"
+#    }
+}#
