b103fc85745fb4ad65cc7aa6856898baa1011029
Peter Deme <snypox@gmail.com>
2022-07-22 15:01:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "random_string" "random" {
 }
 
 resource "random_string" "random2" {
-  length           = 9
+  length           = 10
   special          = true
   override_special = "/@£$"
 }
