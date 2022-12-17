90fce22d45480b8b44c7c7d080c9d6a194894e2c
peterdeme <snypox@gmail.com>
2022-12-17 18:30:09 +0100

    Switch to AWS

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
-#terraform {
-#  required_providers {
-#    aws = { 
-#      source  = "hashicorp/aws"  
-#      version = "~> 3.0"
-#   }
-#  }
-#}
-
-resource "random_string" "random" {
-  length           = 48
-  special          = true
-  override_special = "/@£$"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-#provider "aws" {
-#  region = "eu-central-1"
-#}
+provider "aws" {
+  region = "eu-central-1"
+}
 
-#resource "aws_ssm_parameter" "testparam" {
-#    name = "peterdeme-test-parameter"
-#    type = "String"
-#    value = "foobar"
+resource "aws_ssm_parameter" "testparam" {
+  name  = "peterdeme-test-parameter-2"
+  type  = "String"
+  value = "foobar1"
 
-#    tags = {
-#        developer = "peterdeme"
-#        env_name = "development"
-#    }
-#}
+  tags = {
+    developer = "peterdeme"
+    env_name  = "development"
+  }
+}
+
+# resource "random_string" "random" {
+#   length           = 48
+#   special          = true
+#   override_special = "/@£$"
+# }
