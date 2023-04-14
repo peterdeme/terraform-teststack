8b4a9b3aaa0fb1add1e1bedd7c8b28db09320d5d
Peter Deme <snypox@gmail.com>
2023-04-14 12:22:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "random_pet" "petname" {
 resource "aws_ssm_parameter" "testparam" {
   name  = "peterdeme-test-parameter-2-${random_pet.petname.id}"
   type  = "String"
-  value = "foobar1"
+  value = "foobar12"
 
   tags = {
     developer = "peterdeme"
